cd7077567e82768c108c12a2e9d710249c3f5d36
Nikolaj Sokolowski <n.sokolowski@neusta.de>
2025-02-13 08:49:03 +0100

    Build problems with linked package 9

---
+++ packages/smooth-waves/dist/index.d.ts
@@ -0,0 +1,40 @@
+import * as react_jsx_runtime from 'react/jsx-runtime';
+
+type SupportedEdgeUnit = 'px' | 'vw' | 'vh' | '%';
+type EdgeUnit = `${number}${SupportedEdgeUnit}`;
+type NamedEdges = 'start' | 'end' | 'center';
+type EdgeString = NamedEdges | EdgeUnit | `${number}`;
+type Edge = EdgeString | number;
+type ProgressIntersection = [number, number];
+type Intersection = `${Edge} ${Edge}`;
+type ScrollOffset = Array<Edge | Intersection | ProgressIntersection>;
+/**
+ * Bezier config is an array of 3 numbers
+ * [y-coordinate, x-offset, y-offset]
+ * y-coordinate: The y coordinate of the control point
+ * x-offset: The horizontal shift of the control point
+ * y-offset: The vertical shift from the control point
+ */
+type BezierConfig = [number, number, number];
+interface WaveConfig {
+    left: BezierConfig;
+    right: BezierConfig;
+}
+interface WaveAnimation {
+    forceOverlay: boolean;
+    fillStyle: string;
+    strokeStyle?: string;
+    fillStyleOverlay?: string;
+    stable: WaveConfig;
+    in: WaveConfig;
+    out: WaveConfig;
+    scrollOffset?: ScrollOffset;
+    curveAmount?: number;
+    offsetLeft?: number;
+    offsetRight?: number;
+}
+declare function Wave({ waveConfig: curveConfig }: {
+    waveConfig?: WaveAnimation;
+}): react_jsx_runtime.JSX.Element;
+
+export { Wave, type WaveAnimation, type WaveConfig };
